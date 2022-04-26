bb99c4b0e142c112e35b17e020802f63c55375eb
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-04-26 17:26:43 +0100

    Iteration 2 complete

---
+++ public/scripts/nav-detail.js
@@ -0,0 +1,11 @@
+if (window.location.pathname === "/"){
+    document.getElementById("nav-home").classList.add("current-page");
+}
+
+if (window.location.pathname === "/beers"){
+    document.getElementById("nav-beers").classList.add("current-page");
+}
+
+if (window.location.pathname === "/random-beer"){
+    document.getElementById("nav-random-beer").classList.add("current-page");
+}
